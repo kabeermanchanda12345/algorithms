06550536da535eb0962a99f942cc432a741e3f4f
Kabeer Manchanda <kabeermanchanda@gmail.com>
2014-02-16 22:12:49 -0800

    Creating two weighted Quick union arrays

--- Percolation/Percolation.java
+++ Percolation/Percolation.java
@@ -1,66 +1,112 @@
+
 public class Percolation  {
-	private WeightedQuickUnionUF mConnections;
-	private STATE[][] mData;
-	private int N;
-	// create N-by-N grid, with all sites blocked
-	public Percolation(int N) { 
-		//The two extra are virtual nodes used to represent the top row
-		// All the top rows are connected to to the second last virtual node
-		// All the bottom rows are connected to the last node
-		this.N = N;
-		mConnections = new WeightedQuickUnionUF(N * N +2);
-		for (int i = 0; i < N; i++) {
-			mConnections.union(i, N*N);
-			mConnections.union((N-1)*(N)+i, N*N+1);
-		}
-		
-		mData = new STATE[N][N];
-		for (int i = 0; i < N; i++) {
-			for (int j = 0; j < N; j++) {
-				mData[i][j] = STATE.BLOCK;
-			}
-		}
-	}
-	// open site (row i, column j) if it is not already
-	public void open(int i, int j) {
-		mData[i-1][j-1] = STATE.OPEN;
-		//Left Neighbor
-		if (j-2 >= 0 && mData[i-1][j-2].ordinal() == STATE.OPEN.ordinal()) {
-			mConnections.union((i-1)*(N)+j-2, (i-1)*(N)+j-1);
-		}
-		//Right Neighbor
-		if (j <= N-1 && mData[i-1][j].ordinal() == STATE.OPEN.ordinal()) {
-			mConnections.union((i-1)*(N)+j-1, (i-1)*(N)+j);
-		}
-		
-		//Top Neighbor
-		if (i-2 >= 0 && mData[i-2][j-1].ordinal() == STATE.OPEN.ordinal()) {
-			mConnections.union((i-2)*(N)+j-1, (i-1)*(N)+j-1);
-		}
-		
-		//Bottom Neighbor
-		if (i <= N-1 && mData[i][j-1].ordinal() == STATE.OPEN.ordinal()) {
-			mConnections.union((i-1)*(N)+j-1, (i)*(N)+j-1);
-		}
-	}
-	
-	
-	// is site (row i, column j) open?
-	public boolean isOpen(int i, int j) {
-		return mData[i-1][j-1].ordinal() == STATE.OPEN.ordinal();
-	}
-	
-	// is site (row i, column j) full?
-	public boolean isFull(int i, int j)  {
-		return mConnections.connected((i-1)*N+(j-1), N*N);
-	}
-	// does the system percolate?
-	public boolean percolates()   {
-		return mConnections.connected(N*N+1, N*N);
-	}
-	
-	private enum STATE {
-		BLOCK,
-		OPEN,
-	}
+  private WeightedQuickUnionUF mConnections;
+  private WeightedQuickUnionUF mConnectionsPercolates;
+  private STATE[][] mData;
+  private int N;
+  // create N-by-N grid, with all sites blocked
+  public Percolation(int N) { 
+    //The two extra are virtual nodes used to represent the top row
+    // All the top rows are connected to to the second last virtual node
+    // All the bottom rows are connected to the last node
+    this.N = N;
+    mConnections = new WeightedQuickUnionUF(N * N +2);
+    mConnectionsPercolates = new WeightedQuickUnionUF(N * N +2);
+
+    mData = new STATE[N][N];
+    for (int i = 0; i < N; i++) {
+      for (int j = 0; j < N; j++) {
+        mData[i][j] = STATE.BLOCK;
+      }
+    }
+  }
+  // open site (row i, column j) if it is not already
+  public void open(int i, int j) {
+    checkAndThrow(i, j);
+
+
+    mData[i-1][j-1] = STATE.OPEN;
+
+    if (i == 1) {
+      mConnections.union(j-1, N*N);
+      mConnectionsPercolates.union(j-1, N*N);
+    }
+
+    if (i == N) {
+      mConnectionsPercolates.union((N-1)*(N)+j-1, N*N+1);
+    }
+
+    //Left Neighbor
+    if (j-2 >= 0 && mData[i-1][j-2].ordinal() == STATE.OPEN.ordinal()) {
+      mConnections.union((i-1)*(N)+j-2, (i-1)*(N)+j-1);
+      mConnectionsPercolates.union((i-1)*(N)+j-2, (i-1)*(N)+j-1);
+    }
+    //Right Neighbor
+    if (j <= N-1 && mData[i-1][j].ordinal() == STATE.OPEN.ordinal()) {
+      mConnections.union((i-1)*(N)+j-1, (i-1)*(N)+j);
+      mConnectionsPercolates.union((i-1)*(N)+j-1, (i-1)*(N)+j);
+    }
+
+    //Top Neighbor
+    if (i-2 >= 0 && mData[i-2][j-1].ordinal() == STATE.OPEN.ordinal()) {
+      mConnections.union((i-2)*(N)+j-1, (i-1)*(N)+j-1);
+      mConnectionsPercolates.union((i-2)*(N)+j-1, (i-1)*(N)+j-1);
+    }
+
+    //Bottom Neighbor
+    if (i <= N-1 && mData[i][j-1].ordinal() == STATE.OPEN.ordinal()) {
+      mConnections.union((i-1)*(N)+j-1, (i)*(N)+j-1);
+      mConnectionsPercolates.union((i-1)*(N)+j-1, (i)*(N)+j-1);
+    }
+
+  }
+
+
+  // is site (row i, column j) open?
+  public boolean isOpen(int i, int j) {
+    checkAndThrow(i, j);
+    return mData[i-1][j-1].ordinal() == STATE.OPEN.ordinal();
+  }
+
+  // is site (row i, column j) full?
+  public boolean isFull(int i, int j)  {
+    checkAndThrow(i, j);
+    if (!isOpen(i, j)) return false;
+    return mConnections.connected((i-1)*N+(j-1), N*N);
+  }
+
+  private void checkAndThrow(int i, int j) {
+    if (i < 0 || j > N) {
+      throw new IndexOutOfBoundsException();
+    }
+  }
+  // does the system percolate?
+  public boolean percolates()   {
+    //    boolean iO = false;
+    //    for (int i = 1; i <= N; i++) {
+    //      iO |= isOpen(1, i);
+    //      if (iO) break;
+    //    }
+    //    
+    //    if (!iO) return false;
+    //
+    //    iO = false;
+    //    for (int i = 1; i <= N; i++) {
+    //      iO |= isOpen(N, i);
+    //    }
+    //    if (!iO) return false;
+    //    boolean connected = false;
+    // 
+    //    for (int i = 0; i < N; i++) {
+    //        connected |= mConnections.connected((N-1)*N+i, N*N);
+    //        if (connected) break;
+    //    }
+
+    return mConnectionsPercolates.connected(N*N, N*N+1);
+  }
+
+  private enum STATE {
+    BLOCK,
+      OPEN,
+  }
 }
--- Percolation/PercolationStats.java
+++ Percolation/PercolationStats.java
@@ -1,49 +1,50 @@
-
 public class PercolationStats {
-	private double[] mProbabilities;
-   public PercolationStats(int N, int T) {
-	   mProbabilities = new double[T];
-	   // perform T independent computational experiments on an N-by-N grid
-	   for (int i = 0; i < T; i++) {
-		   Percolation p = new Percolation(N);
-		   int numberOpened = 0;
-		   while (!p.percolates()) {
-			   
-			int x = (int) (1+StdRandom.random() * (N));
-		   	int y = (int) (1+StdRandom.random() * (N));
-		   	if (!p.isOpen(x, y)) {
-		   		p.open(x, y);
-		   		numberOpened += 1;
-		   	}
-		   }
-		   mProbabilities[i] = ((double) numberOpened/(N*N));
-	   }
-   }
-   public double mean() {
-	   return StdStats.mean(mProbabilities);
-   }
-   public double stddev()     {
-	   return StdStats.stddev(mProbabilities);
-   }
-   public double confidenceLo()     {
-	   // returns lower bound of the 95% confidence interval
-	   double low = mean()-1.96*stddev()/Math.sqrt(mProbabilities.length);
-	   return low;
-   }
-   public double confidenceHi()       {
-	   // returns upper bound of the 95% confidence interval
-	   double high = mean()+1.96*stddev()/Math.sqrt(mProbabilities.length);
-	   return high;
-   }
-   public static void main(String[] args)  {
-	   int N = Integer.parseInt(args[0]);
-	   int T = Integer.parseInt(args[1]);
-	   // test client, described below
-	   PercolationStats stats = new PercolationStats(N, T);
-	   System.out.println("Mean= "+stats.mean());
-	   System.out.println("stdev= "+stats.stddev());
-	   System.out.println("95 % confidence= "+stats.confidenceLo()
-			   +" ,"+stats.confidenceHi());
-	   
-   }
+  private double[] mProbabilities;
+  public PercolationStats(int N, int T) {
+    if (N <= 0  || T <= 0) throw new IllegalArgumentException();
+    mProbabilities = new double[T];
+    // perform T independent computational experiments on an N-by-N grid
+    for (int i = 0; i < T; i++) {
+      Percolation p = new Percolation(N);
+      int numberOpened = 0;
+      while (!p.percolates()) {
+        //System.out.println("i="+i+" number= "+numberOpened);
+        int x = (int) (1+StdRandom.random() * (N));
+        int y = (int) (1+StdRandom.random() * (N));
+        if (!p.isOpen(x, y)) {
+          p.open(x, y);
+          numberOpened += 1;
+        }
+      }
+      mProbabilities[i] = ((double) numberOpened/(N*N));
+    }
+  }
+  public double mean() {
+    return StdStats.mean(mProbabilities);
+  }
+  public double stddev()     {
+    return StdStats.stddev(mProbabilities);
+  }
+  public double confidenceLo()     {
+    // returns lower bound of the 95% confidence interval
+    double low = mean()-1.96*stddev()/Math.sqrt(mProbabilities.length);
+    return low;
+  }
+  public double confidenceHi()       {
+    // returns upper bound of the 95% confidence interval
+    double high = mean()+1.96*stddev()/Math.sqrt(mProbabilities.length);
+    return high;
+  }
+  public static void main(String[] args)  {
+    int N = Integer.parseInt(args[0]);
+    int T = Integer.parseInt(args[1]);
+    System.out.println("N= "+N+" T= "+T);
+    // test client, described below
+    PercolationStats stats = new PercolationStats(N, T);
+    System.out.println("Mean= "+stats.mean());
+    System.out.println("stdev= "+stats.stddev());
+    System.out.println("95 % confidence= "+stats.confidenceLo()
+        +" ,"+stats.confidenceHi());
+
+  }
 }
