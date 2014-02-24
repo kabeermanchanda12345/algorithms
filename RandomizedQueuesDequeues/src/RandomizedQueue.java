3639449528dcd1f72dbb7e37863e8784fd71dd69
Kabeer Manchanda <kabeermanchanda@gmail.com>
2014-02-23 20:31:35 -0800

    Optimized commit

--- RandomizedQueuesDequeues/src/RandomizedQueue.java
+++ RandomizedQueuesDequeues/src/RandomizedQueue.java
@@ -1,68 +1,108 @@
-import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.NoSuchElementException;
 
 
 public class RandomizedQueue<Item> implements Iterable<Item> {
 
-    private ArrayList<Item> mItems = new ArrayList<Item>();
+  //  private ArrayList<Item> mItems = new ArrayList<Item>();
+    private ResizingArray<Item> mArray  = new ResizingArray<Item>();
     
     public RandomizedQueue() {
         // construct an empty randomized queue
     }
     public boolean isEmpty() {
         // is the queue empty?
-        return mItems.size() == 0;
+      //  return mItems.size() == 0;
+        return mArray.getLength() == 0;
     }
     public int size() {
         // return the number of items on the queue
-        return mItems.size();
+      //  return mItems.size();
+        return mArray.getLength();
     }
     
     public void enqueue(Item item) {
         if (item == null) throw new NullPointerException();
         // add the item
-        mItems.add(item);
+       // mItems.add(item);
+        mArray.push(item);
     }
     public Item dequeue() {
-        if (mItems.size() == 0) throw new NoSuchElementException();
+        if (mArray.getLength() == 0) throw new NoSuchElementException();
         // delete and return a random item
-        double rand = Math.random()*mItems.size();
-        Item item = mItems.remove(((int) rand));
+        double rand = Math.random()*mArray.getLength();
+        //Item item = mItems.remove(((int) rand));
+        Item item = mArray.pop((int) rand);
         return item;
     }
     public Item sample() {
-        if (mItems.size() == 0) throw new NoSuchElementException();
+        if (mArray.getLength() == 0) throw new NoSuchElementException();
         // return (but do not delete) a random item
-        double rand = Math.random()*mItems.size();
+        double rand = Math.random()*mArray.getLength();
         
-        return mItems.get(((int) rand));
+       // return mItems.get(((int) rand));
+        return mArray.get((int) rand);
     }
     
     public Iterator<Item> iterator() {
-        Iterator<Item> iterator = new RandListIterator((Item[]) mItems.toArray());
+        //Iterator<Item> iterator = new RandListIterator((Item[]) mItems.toArray());
+        Iterator<Item> iterator = new RandListIterator((Item[]) mArray.getArray());
         return iterator;
     }
     
-//    public class ResizingArray<T>  {
-//        private T[] x;
-//        private int N;
-//        public ResizingArray(){
-//            x= (T[])new Object[1];
-//        }
-//        
-//        public void push(T item) {
-//            if(N == x.length) resize(2*x.length);
-//        }
-//        
-//        public void resize(int size) {
-//            T[] copy = (T[]) new Object[size];
-//            for (int i=0; i < N ; i++) {
-//                copy[i] = x[i];
-//            }
-//            x = copy;
-//        }
-//    }
+    private class ResizingArray<T>  {
+        private T[] x;
+        private int N = 0;
+        public ResizingArray() {
+            x = (T[]) new Object[1];
+        }
+        
+        public int getLength() {
+            return N;
+        }
+        
+        public void push(T item) {
+            if (N == x.length) resize(2*x.length);
+            x[N++] = item;
+        }
+        
+        public void resize(int size) {
+            T[] copy = (T[]) new Object[size];
+            for (int i = 0; i < N; i++) {
+                copy[i] = x[i];
+            }
+            x = copy;
+        }
+        
+        public T[] getArray() {
+            T[] copy = (T[]) new Object[N];
+            for (int i = 0; i < N; i++) {
+                copy[i] = x[i];
+            } 
+            return copy;
+        }
+        
+        public T pop(int i) {
+            T item = (T) (new Object());
+           // T item = x[i];
+           // x[i] = null;
+            swap(i, N-1);
+            item = x[N-1];
+            --N;
+            if (N > 0 && N == x.length/4)resize(x.length/2);
+            return item;
+        }
+        
+        public T get(int i) {
+            return x[i];
+        }
+        
+        private void swap(int i , int j) {
+            T temp = x[i];
+            x[i] = x[j];
+            x[j] = temp;
+        }
+    }
     
     private class RandListIterator implements Iterator<Item> {
 
